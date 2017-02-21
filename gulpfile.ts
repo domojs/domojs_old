5563e1936a4c320b1c3a36d007bf60f7ad7f81df
Nicolas Penin <nicolas.penin@dragon-angel.fr>
2017-02-21 06:39:43 +0100

    moved to single quote string

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,7 @@
 ///<reference path="typings/index.d.ts" />
 
-import * as gulp from "gulp";
-import * as ts from "gulp-typescript";
+import * as gulp from 'gulp';
+import * as ts from 'gulp-typescript';
 import * as fs from 'fs';
 import * as merge from 'merge2';
 import * as less from 'gulp-less';
